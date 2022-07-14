59c7dbb9fc5cd9c3cc158a1361bf169ac04fcf84
vmichailcc <vmichail2022@gmail.com>
2022-07-14 22:07:25 +0300

    new DB and update admin.py

--- pet/store/admin.py
+++ pet/store/admin.py
@@ -7,23 +7,18 @@ from .models import ProductCard, Category, Pictures
 class ProductCardAdmin(admin.ModelAdmin):
     list_display = ("get_image", "name", "vendor_code", "brand", "category", "price", "old_price", "availability",
                     "is_published", "card_views")
+    list_filter = ("brand", "category", "price", "availability", "is_published", "card_views")
+    list_editable = ("is_published",)
+    search_fields = ["name", "description", "brand"]
 
     def get_image(self, obj):
         if obj.main_picture:
             return mark_safe(f"<img src='{obj.main_picture}' width='75'>")
         else:
             return "Без фото"
-
     get_image.short_description = "Фото"
 
 
-    """
-    From SPro admin:
-    search_fields = ["title", "description", "author_id"]
-    list_filter = ["title", "author_id"]
-    list_editable = ["title", "description"]
-    radio_fields = {"author_id": admin.HORIZONTAL}
-    """
 admin.site.register(ProductCard, ProductCardAdmin)
 admin.site.register(Category)
 admin.site.register(Pictures)

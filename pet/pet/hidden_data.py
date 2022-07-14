75a316cf0f62889367e8866d3c8c22c4f15b3c60
vmichailcc <vmichail2022@gmail.com>
2022-07-14 11:20:23 +0300

    Initial commit

---
+++ pet/pet/hidden_data.py
@@ -0,0 +1,12 @@
+postgresql_data = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pet3_db',
+        'USER': 'postgres',
+        'PASSWORD': '141215',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+django_secret_key = 'django-insecure-$guem6z%qgl$dvi)6c%=vxr2&6s(5vb8&04e(fj3zk3f^_6l@6'

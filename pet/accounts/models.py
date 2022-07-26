81e56bbccccd9da23131b1c6eef6d45763d39cf6
vmichailcc <vmichail2022@gmail.com>
2022-07-26 21:03:45 +0300

    create superuser and user v2

--- pet/accounts/models.py
+++ pet/accounts/models.py
@@ -58,17 +58,33 @@ from django.contrib.auth.models import AbstractUser
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, email, password, **extra_fields):
+    def _create_user(self, username, email, password, **extra_fields):
         """
         Create and save a user with the given username, email, and password.
         """
         if not email:
             raise ValueError("The given username must be set")
+        username = None
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
         return user
+
+    def create_user(self, username=None, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username=None, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self._create_user(username, email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
@@ -80,4 +96,4 @@ class CustomUser(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    # objects = UserManager()
+    objects = UserManager()

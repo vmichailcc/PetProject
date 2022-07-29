337fcca965f94a441026d69e388035ea6e418e49
vmichailcc <vmichail2022@gmail.com>
2022-07-29 21:50:30 +0300

    end on LikeApiView

--- pet/store/views.py
+++ pet/store/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import ProductCard, ProductComment, Pictures
 from django.views.generic.base import View
 from .forms import AddProductComment
@@ -7,7 +9,6 @@ from .serializers import ProductCardSerializer, ProductCommentSerializer
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from accounts.models import CustomUser
 
 
 class ProductView(ListView):
@@ -39,9 +40,9 @@ class StoreApiView(ListModelMixin, GenericViewSet):
 
 
 class CommentApiView(ModelViewSet):
-    queryset = ProductCard.
     serializer_class = ProductCommentSerializer
     permission_classes = [IsAuthenticated]
+    http_method_names = ['get', 'post']
 
     def perform_create(self, serializer):
         if self.request.user.is_authenticated:
@@ -54,13 +55,14 @@ class CommentApiView(ModelViewSet):
 
 class LikeApiView(ModelViewSet):
     serializer_class = ProductCardSerializer
+    queryset = ProductCard.objects.all()
+    http_method_names = ['get', 'post']
 
-    def perform_create(self, serializer):
-        if self.request.prod.like:
-            # like +=1
-            serializer.save(**{'like': +1 })
-
-    def get_queryset(self):
-        pk = self.request.prod
-        return ProductCard.objects.get(pk=pk)
-
+    @action(detail=True, methods=['post'])
+    def like_as_active(self, request, pk=None):
+        prod = self.get_object()
+        print("!!! prod = ", prod)
+        prod.like += 1
+        prod.save()
+        serializer = self.get_serializer(prod)
+        return Response(serializer.data)
